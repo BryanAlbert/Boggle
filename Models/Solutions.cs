d4623a0e1c7d5f5025a2a989ca6187647f38245a
Bryan Albert <bryanalbert@hotmail.com>
2023-05-23 14:57:31 -0700

    Test data with ListView on Words page.

---
+++ Models/Solutions.cs
@@ -0,0 +1,15 @@
+﻿namespace Boggle.Models
+{
+	internal class Solutions : List<Solution>
+	{
+		public Solutions(int wordLength, List<Solution> solutions) : base(solutions)
+		{
+			WordLength = wordLength;
+		}
+
+
+		public int WordLength { get; set; }
+		public int Score { get; set; }
+		public int TotalScore { get; set; }
+	}
+}
--- ViewModels/WordsViewModel.cs
+++ ViewModels/WordsViewModel.cs
@@ -14,7 +14,7 @@ namespace Boggle.ViewModels
 			try
 			{
 				m_solver = new Solver();
-				Solutions = new ObservableCollection<Solution>();
+				Solutions = new ObservableCollection<Solutions>();
 				WeakReferenceMessenger.Default.Register<GameViewModel>(this, OnGameUpdated);
 				_ = WeakReferenceMessenger.Default.Send(App.c_isGameSelected);
 				_ = WeakReferenceMessenger.Default.Send(App.c_isBoardGenerated);
@@ -35,7 +35,7 @@ namespace Boggle.ViewModels
 		public string Message { get => m_message; set => SetProperty(ref m_message, value); }
 		public string Name { get => m_name; set => SetProperty(ref m_name, value); }
 		public int Score { get => m_score; set => SetProperty(ref m_score, value); }
-		public ObservableCollection<Solution> Solutions { get; private set; }
+		public ObservableCollection<Solutions> Solutions { get; private set; }
 		public bool BoardGenerated { get => m_boardGenerated; set => SetProperty(ref m_boardGenerated, value); }
 		public bool Solved { get => m_solved; private set => SetProperty(ref m_solved, value); }
 		public ICommand SolveCommand { get; set; }
@@ -56,11 +56,30 @@ namespace Boggle.ViewModels
 			// TODO: figure out how to use the RelayCommand constructor that takes the Func<bool> canExecute parameter,
 			if (BoardGenerated && !m_solved)
 			{
+#if false
 				foreach (Solution solution in m_solver.Solve())
 					Solutions.Add(solution);
 			
 				Solved = true;
 				Score = Solutions.Sum(x => x.Score);
+#else
+
+				// TODO: testing
+				GameViewModel game = new(new Game() { Scoring = new List<string> { "0", "0", "1", "1", "2", "3", "5", "11" } });
+				List<Solution> solutions = new List<Solution>
+				{
+					new Solution("FRED", new int[] { 4, 5, 6, 11 }, game),
+					new Solution("FART", new int[] { 4, 5, 6, 11 }, game)
+				};
+
+				Solutions.Add(new Solutions(4, solutions));
+				solutions.Clear();
+				solutions.Add(new Solution("BRUCE", new int[] { 0, 1, 2, 3, 7 }, game));
+				solutions.Add(new Solution("ONION", new int[] { 0, 1, 2, 3, 7 }, game));
+				Solutions.Add(new Solutions(5, solutions));
+				Solved = true;
+				Score = Solutions.Sum(x => x.Score);
+#endif
 			}
 		}
 
