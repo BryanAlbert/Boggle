6cf167519a455eb2f6b757e7574e01f86c66936f
Bryan Albert <bryanalbert@hotmail.com>
2023-06-23 14:50:53 -0700

    CopyInstallersToPublic catches different Solution Platform publishments (e.g. x64 instead of Any).

--- CopyInstallersToPublic.ps1
+++ CopyInstallersToPublic.ps1
@@ -2,6 +2,8 @@ param([Parameter(HelpMessage = "Overwrite existing installer files")] [switch] $
 
 # Copies the latest AppPackages to the Public Downloads folder.
 
+$solutionName = Split-Path $PWD -Leaf
+
 $windowsManifest = ".\Platforms\Windows\Package.appxmanifest"
 $windowsAppPackages = ".\bin\Release\net7.0-windows10.0.19041.0\win10-x64\AppPackages"
 $androidManifest = ".\Platforms\Android\AndroidManifest.xml"
@@ -16,46 +18,53 @@ if ($version.Substring(0, $version.LastIndexOf(".")) -ne $versionName)
     Write-Host "IncrementBuildNumber.ps1 updates both version strings."
 }
 
-$windowsSource = (Get-ChildItem $windowsAppPackages *$version* -Directory)
+$windowsSource = Get-ChildItem $windowsAppPackages *$version* -Directory
+if ($windowsSource.Count -gt 1)
+{
+    Write-Host "Error: $windowsAppPackages has $($windowsSource.Count) folders with version $version"
+    return
+}
+
 $androidSource = (Get-ChildItem $androidAppPackages *$versionName.apk)
 $debug = $windowsSource | Where-Object { $_.Name.Contains("_Debug_") }
 if ($null -ne $debug)
 {
     Write-Host "Error: _Debug_ folder found. Did you build Release? Folder(s):"
     $windowsSource
+    return
 }
-elseif ($null -ne $windowsSource)
+
+if ($null -ne $windowsSource)
 {
     if ($null -eq $androidSource)
     {
         Write-Host "Error: directory for version $versionName not found in $androidAppPackages"
+        return
     }
-    else
+
+    $target = Join-Path $public (Split-Path $windowsSource -Leaf)
+    if (Test-Path $target)
     {
-        $target = Join-Path $public (Split-Path $windowsSource -Leaf)
-        if (Test-Path $target)
+        if ($Overwrite)
         {
-            if ($Overwrite)
-            {
-                Write-Host "Deleting $target and copying $windowsSource to $public..."
-                Remove-Item $target -Recurse 
-                Copy-Item $windowsSource -Recurse -Destination $public
-                Write-Host "Copying $androidSource directory to $public..."
-                Copy-Item $androidSource -Destination $public
-            }
-            else
-            {
-                Write-Host "Error: directory already exists: $target"
-                Write-Host "Did you run IncrementBuildNumber.ps1 and Publish Windows and Android?"
-            }
-        }
-        else
-        {
-            Write-Host "Copying $windowsSource directory to $public..."
+            Write-Host "Deleting $target and copying $windowsSource to $public..."
+            Remove-Item $target -Recurse 
             Copy-Item $windowsSource -Recurse -Destination $public
             Write-Host "Copying $androidSource directory to $public..."
             Copy-Item $androidSource -Destination $public
         }
+        else
+        {
+            Write-Host "Error: directory already exists: $target"
+            Write-Host "Did you run IncrementBuildNumber.ps1 and Publish Windows and Android?"
+        }
+    }
+    else
+    {
+        Write-Host "Copying $windowsSource directory to $public..."
+        Copy-Item $windowsSource -Recurse -Destination $public
+        Write-Host "Copying $androidSource directory to $public..."
+        Copy-Item $androidSource -Destination $public
     }
 }
 else
